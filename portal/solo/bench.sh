b05c5987de802d7ff4235453e0c8439e5764d291
gongqi.zhen <gongqi.zhen@gmail.com>
2023-09-25 00:15:24 +0900

    app.go

--- portal/solo/bench.sh
+++ portal/solo/bench.sh
@@ -3,6 +3,10 @@
 # log rotate
 docker exec -it webapp-nginx-1 /bin/bash /var/log/nginx/nginx-logrotate.sh
 
+# /dev/nullをコピーしているが
+# rm mysql-slow.log && mysqladmin flush-logs; でも良い
+docker exec -it webapp-mysql-1 /bin/bash -c "cp /dev/null /var/log/mysql/mysql-slow.log"
+
 # benchmark
 readonly GAS_POST_URL="https://script.google.com/macros/s/AKfycbzAtaKnPpjujtHZYBpKkvgmd72EtHHceC9EvlZWePScjNEQgQB9w64OuifC8T69YAc/exec"
 readonly TARGET_URL="http://localhost"
