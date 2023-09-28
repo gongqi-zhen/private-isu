67502b2ee05f8f02273b7edbbcdd62be26ac363a
gongqi.zhen <gongqi.zhen@gmail.com>
2023-09-29 00:13:09 +0900

    Revert "sessionをCookieに持たせる"
    This reverts commit 57c1ec510be82764d8ac78f8a1418dfe17de6275.
    
    benchmarkerも同居させているのでスコアが下がった
    環境にあったチューニングをするため一度revertする

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -19,6 +19,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/bradfitz/gomemcache/memcache"
+	gsm "github.com/bradleypeabody/gorilla-sessions-memcache"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	_ "github.com/go-sql-driver/mysql"
@@ -28,7 +30,7 @@ import (
 
 var (
 	db    *sqlx.DB
-	store sessions.Store
+	store *gsm.MemcacheStore
 )
 
 const (
@@ -84,7 +86,12 @@ type Comment struct {
 }
 
 func init() {
-	store = sessions.NewCookieStore([]byte("sendagaya"))
+	memdAddr := os.Getenv("ISUCONP_MEMCACHED_ADDRESS")
+	if memdAddr == "" {
+		memdAddr = "localhost:11211"
+	}
+	memcacheClient := memcache.New(memdAddr)
+	store = gsm.NewMemcacheStore(memcacheClient, "iscogram_", []byte("sendagaya"))
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 }
 
